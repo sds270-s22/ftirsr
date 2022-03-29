ef1176a402ff548daabd0c9b67531f5414723251
Grace Hartley <grace.hartley@gmail.com>
2022-03-29 12:02:26 -0400

    change doc

--- R/data.R
+++ R/data.R
@@ -6,7 +6,8 @@
 #' \describe{
 #' \item{dataset}{Core Sample ID}
 #' \item{BSiPercent}{Percentage of Biogenic Silica in Sample}
-#' \item{V1,...,V3697}{Location along Wavenumber Spectrum}
+#' \item{V1}{Location along Wavenumber Spectrum}
+#'
 #' }
 #' @source Gathered from <https://github.com/sds-capstone/PLSmodel>
 "greenland"
