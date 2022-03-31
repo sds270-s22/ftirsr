e70cf26afa9985847448f70a5dbe1727813fb9f4
evejcik <47393221+evejcik@users.noreply.github.com>
2022-03-31 10:35:42 -0400

    Moving wetChemAbsorbance to data-raw, adding to read_csv

--- data-raw/greenland.R
+++ data-raw/greenland.R
@@ -1,7 +1,7 @@
 ## code to prepare `greenland` dataset goes here
 library(tidyverse)
 library(dplyr)
-greenland <- read_csv("wetChemAbsorbance.csv")%>%
+greenland <- read_csv("data-raw/wetChemAbsorbance.csv")%>%
   select(-`...3`)
 usethis::use_data(greenland, overwrite = TRUE)
 
