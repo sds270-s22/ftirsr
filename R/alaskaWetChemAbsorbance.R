476006db0dd8f90c57198b328727e1519abcddbb
Grace Hartley <grace.hartley@gmail.com>
2022-04-12 11:44:34 -0400

    updated file location

--- R/alaskaWetChemAbsorbance.R
+++
@@ -1,8 +0,0 @@
-## code to prepare `alaskaWetChemAbsorbance` dataset goes here
-library(dplyr)
-library(readr)
-library(janitor)
-alaskaWetChemAbsorbance <- read_csv("data-raw/AlaskaWetChemAbsorbance.csv")%>%
-  clean_names()%>%
-  rename(bsi_percent = b_si_percent, sample_id = dataset)
-usethis::use_data(alaskaWetChemAbsorbance, overwrite = TRUE)
