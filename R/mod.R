b55d8ac2bcc303db799da989924a02fff5f23744
Grace Hartley <grace.hartley@gmail.com>
2022-04-14 11:46:54 -0400

    update mod

--- R/mod.R
+++ R/mod.R
@@ -4,6 +4,14 @@ library(pls)
 bsi_mod <- plsr(bsi_percent~., ncomp = 10, data = greenland, validation = "CV", segments = 10)
 summary(bsi_mod)
 
+build_mod <- function(x){
+  # need to vectorize this!
+  if(x == greenland){
+    pivot_longer()
+  }
+}
+
+
 bsi_mod <- function(...){
 
   plsr(ncomp = 10)
