7d30f0bca8bb0d791dd1ae0ae9c51177a77622b7
evejcik <47393221+evejcik@users.noreply.github.com>
2022-04-19 11:46:32 -0400

    alaska edits

--- data-raw/alaskaWetChemAbsorbance.R
+++ data-raw/alaskaWetChemAbsorbance.R
@@ -8,7 +8,7 @@ alaskaWetChemAbsorbance <- read_csv("data-raw/AlaskaWetChemAbsorbance.csv")%>%
   clean_names() %>%
 rename(bsi_percent = b_si_percent, sample_id = dataset)%>%
   pivot_longer(
-  3:1882,
+  3:1884,
   names_to = "wavenumbers",
   values_to = "absorbance")
 usethis::use_data(alaskaWetChemAbsorbance, overwrite = TRUE)
