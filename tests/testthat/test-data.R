28053105853a6733dc290c67c7a0e85625fddecc
evejcik <47393221+evejcik@users.noreply.github.com>
2022-04-19 11:55:55 -0400

    updating tests for datasets

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -2,25 +2,25 @@ library(testthat)
 
 #greenland.csv unit tests
 test_that("Checking greenland rows, cols", {
-  expect_equal(dim(greenland)[1], 52696)
-  expect_equal(dim(greenland)[2], 1819)
+  expect_equal(dim(greenland)[1], 103516)
+  expect_equal(dim(greenland)[2], 4)
 })
 
 test_that("Checking classes", {
   expect_is(greenland$bsi_percent, "numeric")
   expect_is(greenland$sample_id, "character")
-  expect_is(greenland$v1, "numeric")
+  expect_is(greenland$absorbance, "numeric")
 })
 
 #alaskaWetChemAbsorbance.csv unit tests
 test_that("Checking alaskaWetChemAbsorbance rows, cols", {
-  expect_equal(dim(alaskaWetChemAbsorbance)[1], 193640)
-  expect_equal(dim(alaskaWetChemAbsorbance)[2], 6)
+  expect_equal(dim(alaskaWetChemAbsorbance)[1], 193846)
+  expect_equal(dim(alaskaWetChemAbsorbance)[2], 4)
 })
 
 test_that("Checking classes", {
   expect_is(alaskaWetChemAbsorbance$bsi_percent, "numeric")
   expect_is(alaskaWetChemAbsorbance$sample_id, "character")
-  expect_is(alaskaWetChemAbsorbance$v1, "numeric")
+  expect_is(alaskaWetChemAbsorbance$absorbance, "numeric")
 })
 
