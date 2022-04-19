a2fb84120e21f7de0810bbd34ff4604388baaae1
Grace Hartley <grace.hartley@gmail.com>
2022-04-19 11:43:36 -0400

    merge conflict fix

--- data-raw/greenland.R
+++ data-raw/greenland.R
@@ -3,13 +3,13 @@ library(dplyr)
 library(janitor)
 library(tidyr)
 library(readr)
+library(magrittr)
 greenland <- read_csv("data-raw/wetChemAbsorbance.csv")%>%
   select(-`...3`) %>%
   clean_names() %>%
   rename(bsi_percent = b_si_percent, sample_id = dataset)%>%
   pivot_longer(
-    3:1884,
+    3:3699,
     names_to = "wavenumbers",
     values_to = "absorbance")
 usethis::use_data(greenland, overwrite = TRUE)
-
