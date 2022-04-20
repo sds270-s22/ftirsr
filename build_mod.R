10b79367c0b9b1ea0e1756f689419eb392c11562
Grace Hartley <grace.hartley@gmail.com>
2022-04-20 19:45:18 -0400

    fixed mixed models

--- build_mod.R
+++ build_mod.R
@@ -10,7 +10,8 @@ greenland_df <- read_ftirs("~/PLSmodel/Samples/greenland_csv",
 )
 alaska_df <- read_ftirs("~/PLSmodel/Samples/alaska_csv",
                         "~/PLSmodel/csvFiles/AlaskaWetChem.csv"
-)  # this is missing one BSi value?
+) %>%  # this is missing one BSi value?
+  filter(sample_id!="AS-68 (8_24_16).0" )
 
 combined_artic_df_wide <- rbind(greenland_df, alaska_df) %>%
   pivot_ftirs_wider()%>%
@@ -18,16 +19,37 @@ combined_artic_df_wide <- rbind(greenland_df, alaska_df) %>%
 
 # generating just greenland in case full spectrum of both combined is the issue
 greenland_wide <- greenland_df %>%
-  pivot_ftirs_wider() %>%
+  pivot_ftirs_wider()%>%
   select(-1883)
 
-greenland_mod <- plsr(bsi~., ncomp = 10, data = greenland_wide, validation = "CV", segments = 10)
-summary(greenland_mod)
+alaska_wide <- alaska_df %>%
+  pivot_ftirs_wider()%>%
+  select(-1883)
 
+
+greenland_mod <- plsr(bsi~., ncomp = 10, data = greenland_wide, validation = "CV", segments = 10)
+
+ak_mod <- plsr(bsi~., ncomp = 10, data = alaska_wide, validation = "CV", segments = 10)
+
+combined_mod <- plsr(bsi~., ncomp = 10, data = combined_artic_df_wide, validation = "CV", segments = 10)
+
+#these work
 predict(greenland_mod, greenland_wide %>%select(-1))
-predict(greenland_mod, your_data)
+predict(ak_mod, alaska_wide %>%select(-1))
+predict(ak_mod, your_data)
+predict(combined_mod, combined_artic_df_wide%>%select(-1))
+
+# these don't work
+predict(ak_mod, data = greenland_wide %>%select(-1))
+predict(greenland_mod, data =  your_data)
+predict(greenland_mod, data = combined_artic_df_wide%>%select(-1))
+predict(ak_mod, data = combined_artic_df_wide%>%select(-1))
+predict(greenland_mod, data = alaska_wide%>%select(-1))
+
+
 # Hypothetical data our user has (it's just alaska)
 your_data <- read_ftirs("~/PLSmodel/Samples/alaska_csv") %>%
+  filter(sample_id != "AS-68 (8_24_16).0")%>%
   pivot_ftirs_wider() %>%
   select(-1882)
 
@@ -39,6 +61,7 @@ create_new_mod <- function(...){
 
 # to predict your samples with our model
 # should call bsi or something more generic?
+# right now filtering out NA but instead need to find that BSi value for AK
 predict_bsi <- function(your_data, ...){
   # I'm confused about what is being predicted, whether predicting with our
   # data or your data
