e6c606d759f6968b5c6d04a5d1bc0d220578744e
Grace Hartley <grace.hartley@gmail.com>
2022-04-26 11:23:20 -0400

    passing dots

--- R/ftirs.R
+++ R/ftirs.R
@@ -88,9 +88,8 @@ read_ftirs <- function(dir_path, wet_chem_path = NULL, format = "long", ...) {
 #' @importFrom readr read_csv
 #' @importFrom magrittr %>%
 #' @import dplyr
-#' @export
 
-read_wet_chem <- function(filepath, data) {
+read_wet_chem <- function(filepath, data, ...) {
   wet_chem <- read_csv(filepath)
 
   sample_col_name <- names(wet_chem)[1]
@@ -173,29 +172,22 @@ is.ftirs <- function(obj, ...) {
 #' @rdname ftirs
 #' @param your_data must be in the wide format -> looks like it might not have to be!
 #' @param ... Other arguments passed on to methods. Not currently used.
+#' @import pls
 #' @importFrom tibble rownames_to_column
-#' @importFrom magrittr %>%
 #' @importFrom stats predict
 #' @export
 
 predict.ftirs <- function(your_data, ...) {
-  mod <- arctic_mod()
-  preds <- as.data.frame(predict(mod, data = your_data))
-  ## include sample id
-  # not sure if these are the right predictions?
+  combined_artic_df_wide <- rbind(greenland, alaska) %>%
+    pivot_wider()
+
+  our_mod <- plsr(bsi ~ ., ncomp = 10, data = combined_artic_df_wide, validation = "CV", segments = 10)
+
+
+  preds <- as.data.frame(predict(our_mod, data = your_data)) %>%
+    rownames_to_column(var = "sample_id")
+  ## these are the wrong sample_id names
   # eventually just return for component we want
 
   # predplot(our_mod, ncomp = 10, newdata =  your_data, asp = 1, line = TRUE)
-}
-
-#' A function that returns the PLSR model used by `predict.ftirs()` to predict BSi percentages from testing data.
-#' This model is trained on arctic lake core samples from Alaska and Greenland.
-#' @param ... Other arguments passed on to methods. Not currently used.
-#' @importFrom pls plsr
-#' @export
-
-arctic_mod <- function(...){
-  combined_arctic_df_wide <- rbind(greenland, alaska)
-  our_mod <- plsr(bsi ~ ., ncomp = 10, data = combined_arctic_df_wide,
-                  validation = "CV", segments = 10)
 }
