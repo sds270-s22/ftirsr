6c947fb426bf8d6a7ffa24ede6fad2542d2a1d2b
Grace Hartley <grace.hartley@gmail.com>
2022-04-28 14:16:29 -0400

    trying to fix the rounded wavenumbers

--- data-raw/rounded_wavenumbers.R
+++ data-raw/rounded_wavenumbers.R
@@ -3,7 +3,7 @@ library(dplyr)
 library(readr)
 library(magrittr)
 
-wavenumbers <- read_csv("data-raw/AS-01\ (8_24_16).0.csv")
+wavenumbers <- read_csv("~/ftirsr/data-raw/AS-01\ (8_24_16).0.csv")
 ## This "rounds" the absorbance column to zero, but this column is just a filler
 ## in order to include as a dataset
 rounded_wavenumbers <- round(wavenumbers, digits = 0) %>%
