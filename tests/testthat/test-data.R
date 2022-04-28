8a6f0bda7784c6b088e1f103848feb4238cab95a
evejcik <47393221+evejcik@users.noreply.github.com>
2022-04-27 23:47:25 -0400

    NO ERRORS

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -48,7 +48,7 @@ test_that("Checking read_ftirs_file", {
 
 
 test_that("Checking read_ftirs", {
-  expect_equal(as_ftirs(as_tibble(suppressWarnings(read_ftirs("test_samples")))), head(greenland %>% select(-2), 5643))
+  expect_equal(as.ftirs(as_tibble(suppressWarnings(read_ftirs("test_samples")))), head(greenland %>% select(-2), 5643))
   expect_error(read_ftirs("test_samples/FISK-10.0.csv"))
   expect_equal(ncol(suppressWarnings(read_ftirs("test_samples", "wet-chem-data.csv"))), 4)
   expect_error(suppressWarning(read_ftirs("test_samples", "test_samples")))
