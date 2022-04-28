8a2985538c7f28398b49546eab22f05f39f57f07
evejcik <47393221+evejcik@users.noreply.github.com>
2022-04-28 00:27:53 -0400

    sticker updates

--- data-raw/Sticker/sticker.R
+++ data-raw/Sticker/sticker.R
@@ -1,12 +1,14 @@
 library(hexSticker)
 library(OpenImageR)
 library(magick)
+library(showtext)
 library(plsr)
+
 
 line_chart <- image_read("./data-raw/Sticker/linechart4.png")
 
 sticker(
-  subplot = line_chart, p_size = 20, p_color = "#4365F9", h_color = "#4365F9", h_fill = "#42EB1A",
+  subplot = line_chart, p_size = 10, p_color = "#197692", h_color = "#1D87A6", h_fill = "#8FF776",
   s_x = 1.25, s_y = 0.9, s_width = 2.5, s_height = 2,
   white_around_sticker = FALSE,
   package = "ftiRRRs"
